d2605704e344e0adac55ba1caa1becc2565e53fe
emminyell <140811695+emminyell@users.noreply.github.com>
2023-12-04 17:03:05 +0000

    supprimer reservation

--- reservation_delete.php
+++ reservation_delete.php
@@ -2,12 +2,12 @@
 @include 'cnxDB.php';
 if (isset($_GET['deleteid'])) {
     $id=$_GET['deleteid'];
-    $sql="DELETE FROM `reservation` WHERE reservation_id=$id";
-    $result=mysqli_query($conn,$sql);
+    $idinvoice=$_GET['idinoice'];
+    $delinv="DELETE FROM `invoice`  WHERE invoice_id =$idinvoice";
+    $result=mysqli_query($conn,$delinv);
     if ($result) {
-    header("location:reservation.php");
-    } else {
-        die(mysqli_error($conn));
-    }
-}
+        $delres="DELETE FROM `reservation` WHERE reservation_id =$id";
+        $result=mysqli_query($conn,$delres);  
+        header("location:reservation.php");
+}}
 ?>
